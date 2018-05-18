4aa8dd7dc2681d4ad4e2a60adb3b366c78b33b90
Serhii Mikheiev <mikh.s.o@gmail.com>
2018-05-19 00:00:55 +0200

    set session channel from route

--- client/js/routes.js
+++ client/js/routes.js
@@ -6,5 +6,6 @@ Router.route('/', function() {
 	this.redirect('/general');
 });
 Router.route('/:channel', function() {
+	Session.set('channel', this.params.channel);
 	this.render('messages');
 });
--- client/js/template-channel.js
+++ client/js/template-channel.js
@@ -1,9 +1,3 @@
-Template.channel.events({
-	'click .channel': function(e) {
-		Session.set('channel', this.name);
-	}
-});
-
 Template.channel.helpers({
 	active: function() {
 		if (Session.get('channel') === this.name) {
